4ea6d025ed400ed73640fdde56272742910fee0e
--local <--local>
2020-09-20 21:51:05 -0400

    'working'

--- Tests/Services/RedditTwitterDataAPI/test_app.py
+++ Tests/Services/RedditTwitterDataAPI/test_app.py
@@ -8,6 +8,7 @@ from flask_api import status
 
 from global_parameters import ALL_ASPECTS
 from global_parameters import ALL_CRALWERS
+from global_parameters import ALL_REDDIT_FEILDS
 
 
 @pytest.mark.parametrize(
@@ -62,3 +63,26 @@ def test_aspects_parameter_with_all_value(client):
 
     assert y.json["all_retrived_data"][0]['aspect'] \
            == x.json["all_retrived_data"][0]['aspect']
+
+
+# @pytest.mark.parametrize(
+#     "request_value,responds_value",
+#     [
+#         (i, i) for i in ALL_REDDIT_FEILDS
+#     ],
+# )
+# def test_aspects_parameter(client, request_value, responds_value):
+#     x = client.get(f"/fields?={request_value}")
+#     assert status.HTTP_200_OK == int(x.status.split(" ")[0])
+#     assert f'{responds_value}' \
+#            == x.json["all_retrived_data"][0]['aspect']
+
+# def test_aspects_parameter_with_all_value(client):
+#     concat_crawlers = ','.join(ALL_ASPECTS)
+#     x = client.get("/?aspects=all")
+#     y = client.get(f"/?aspects={concat_crawlers}")
+#     assert status.HTTP_200_OK == int(x.status.split(" ")[0])
+#     assert status.HTTP_200_OK == int(y.status.split(" ")[0])
+#
+#     assert y.json["all_retrived_data"][0]['aspect'] \
+#            == x.json["all_retrived_data"][0]['aspect']
