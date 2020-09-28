3f836d45290be664563d92fe9a7647b2e62d9f51
--local <--local>
2020-09-28 13:22:17 -0400

    TCR

--- Tests/Utilities/test_email_utility.py
+++ Tests/Utilities/test_email_utility.py
@@ -3,25 +3,14 @@
 
 """Test email related utility function."""
 
-
-import smtplib
-import ssl
-
 from credentials import email
-from credentials import password
+from src.Utilities.Emails.email_utility import send_email
 
 
 def test_send_email():
-    port = 465  # For SSL
-    smtp_server = "smtp.gmail.com"
-    sender_email = email
-    receiver_email = email
     # password = input("Type your password and press enter: ")
     message = """\
     Subject: Hi there
 
     This message is sent from Python."""
-    context = ssl.create_default_context()
-    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-        server.login(sender_email, password)
-        server.sendmail(sender_email, receiver_email, message)
+    send_email(message, email)
--- src/Utilities/Emails/email_utility.py
+++ src/Utilities/Emails/email_utility.py
@@ -3,6 +3,19 @@
 
 """Contain email related utility function."""
 
+import smtplib
+import ssl
 
-def send_email():
-    pass
+from credentials import email
+from credentials import password
+
+
+def send_email(message: str, receiver_email: str):
+    port = 465  # For SSL
+    smtp_server = "smtp.gmail.com"
+    sender_email = email
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
