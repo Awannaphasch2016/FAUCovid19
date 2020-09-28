1615f4d382964a9e6167ea4f902cb0c094ac6052
--local <--local>
2020-09-28 17:20:04 -0400

    TCR

--- src/Utilities/Logging/logger.py
+++ src/Utilities/Logging/logger.py
@@ -6,54 +6,56 @@
 import logging
 
 # create logger
-module_logger = logging.getLogger("logger_module.auxiliary")
-
-LOGGER_NAME = 'logger_class'
+module_logger = logging.getLogger("logger_module")
 
 
-class Auxiliary:
+class MyLogger:
+    LOGGER_NAME = 'logger_class'
+
     def __init__(self):
-        self.logger = logging.getLogger(f"{LOGGER_NAME}")
-        self.logger.info("Creating an instance of Auxiliary...")
-        self._prepare_logger()
+        self._prepare_loggers()
 
-    # def do_something(self):
-    #     self.logger.info("doing something")
-    #     a = 1 + 1
-    #     self.logger.info("done doing something")
-    #     self.logger.error("error")
-    #     self.logger.debug("debug")
-
-    def _prepare_logger(self):
+    def _prepare_loggers(self):
+        self.logger = logging.getLogger("spam_application")
         self._init_program_logger()
         self._init_error_logger()
 
     def _init_program_logger(self):
         self.program_logger = logging.StreamHandler()
 
-        logger = logging.getLogger("spam_application")
-        self.program_logger.setLevel(logging.ERROR)
+        self.program_logger.setLevel(logging.INFO)
+        self.program_logger.setLevel(logging.WARNING)
 
         # create formatter and add it to the handlers
         formatter1 = logging.Formatter("%(message)s")
         self.program_logger.setFormatter(formatter1)
 
         # add the handlers to the logger
-        logger.addHandler(self.program_logger)
+        self.logger.addHandler(self.program_logger)
 
     def _init_error_logger(self):
-        self.program_logger = logging.StreamHandler()
+        self.error_logger = logging.StreamHandler()
 
-        logger = logging.getLogger("spam_application")
         self.program_logger.setLevel(logging.ERROR)
-
-        # create formatter and add it to the handlers
         formatter1 = logging.Formatter("%(message)s")
         self.program_logger.setFormatter(formatter1)
 
-        # add the handlers to the logger
-        logger.addHandler(self.program_logger)
+        self.logger.addHandler(self.program_logger)
+
+    def _init_debug_logger(self):
+        self.debug_logger = logging.StreamHandler()
+
+        self.program_logger.setLevel(logging.DEBUG)
+        formatter1 = logging.Formatter("%(message)s")
+        self.program_logger.setFormatter(formatter1)
+
+        self.logger.addHandler(self.program_logger)
+
 
 
 def some_function():
     module_logger.info(f"received a call to {some_function.__name__}")
+
+
+if __name__ == '__main__':
+    pass
