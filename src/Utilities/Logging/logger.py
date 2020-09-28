0f6eb7088116553e7bfa3422c644f2926b4b2501
--local <--local>
2020-09-28 17:39:09 -0400

    TCR

--- src/Utilities/Logging/logger.py
+++ src/Utilities/Logging/logger.py
@@ -10,7 +10,7 @@ from typing import List
 
 
 class MyLogger:
-    LOGGER_NAME = 'logger_class'
+    LOGGER_NAME = 'LoggerClass'
 
     def __init__(self):
         self._prepare_loggers()
@@ -26,38 +26,47 @@ class MyLogger:
         )
         return formatter
 
-    def _set_logger(self,
-                    log_handler: logging.Handler,
-                    log_level: List[int],
-                    ) -> None:
+    def _add_logger_handler(self,
+                            log_handler: logging.Handler,
+                            log_level: List[int],
+                            ) -> None:
         for i in log_level:
             log_handler.setLevel(i)
 
+        # create formatter and add it to the handlers
         log_handler.setFormatter(self._get_formatter())
+
+        # add the handlers to the logger
         self.logger.addHandler(log_handler)
 
     def _init_program_logger(self):
         self.program_logger = logging.StreamHandler()
-
-        self.program_logger.setLevel(logging.INFO)
-        self.program_logger.setLevel(logging.WARNING)
-
-        # create formatter and add it to the handlers
-        self.program_logger.setFormatter(self._get_formatter())
-
-        # add the handlers to the logger
-        self.logger.addHandler(self.program_logger)
+        self._add_logger_handler(
+            self.program_logger,
+            [
+                logging.INFO,
+                logging.WARNING
+            ]
+        )
 
     def _init_error_logger(self):
         self.error_logger = logging.StreamHandler()
+        self._add_logger_handler(
+            self.error_logger,
+            [
+                logging.ERROR,
+            ]
+        )
 
-        self.program_logger.setLevel(logging.ERROR)
-        self.program_logger.setFormatter(self._get_formatter())
-
-        self.logger.addHandler(self.program_logger)
 
     def _init_debug_logger(self):
         self.debug_logger = logging.StreamHandler()
+        self._add_logger_handler(
+            self.debug_logger,
+            [
+                logging.DEBUG,
+            ]
+        )
 
         self.program_logger.setLevel(logging.DEBUG)
         self.program_logger.setFormatter(self._get_formatter())
@@ -65,9 +74,6 @@ class MyLogger:
         self.logger.addHandler(self.program_logger)
 
 
-def some_function():
-    module_logger.info(f"received a call to {some_function.__name__}")
-
-
 if __name__ == '__main__':
+    print('hi')
     pass
