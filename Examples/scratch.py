12d23d8244c54968c873fcc8e26a2090ffa04276
--local <--local>
2020-09-28 12:28:32 -0400

    TCR

--- Examples/scratch.py
+++ Examples/scratch.py
@@ -2,15 +2,16 @@ import smtplib, ssl
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = "awannaphasch2016@fau.edu"  # Enter your address
-receiver_email = "awannaphasch2016@fau.edu"  # Enter receiver address
-password = input("Type your password and press enter: ")
+sender_email = "terngoodod@gmail.com"  # Enter your address
+receiver_email = "terngoodod@gmail.com"  # Enter receiver address
+# password = input("Type your password and press enter: ")
 message = """\
 Subject: Hi there
 
 This message is sent from Python."""
+password = 'Terng2258'
 
-# context = ssl.create_default_context()
-# with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
-#     server.login(sender_email, password)
-#     server.sendmail(sender_email, receiver_email, message)
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
