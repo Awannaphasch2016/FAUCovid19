99deb4a65c3c194cb04fe78650e031fe198deb22
--local <--local>
2020-09-28 12:16:09 -0400

    TCR

--- Examples/scratch.py
+++ Examples/scratch.py
@@ -3,6 +3,15 @@ import smtplib, ssl
 port = 465  # For SSL
 password = input("Type your password and press enter: ")
 
+sender_email = "my@gmail.com"
+receiver_email = "your@gmail.com"
+message = """\
+Subject: Hi there
+
+This message is sent from Python."""
+
+# Send email here
+
 # Create a secure SSL context
 context = ssl.create_default_context()
 
