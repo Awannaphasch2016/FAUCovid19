1b2492606b816dce3cb8e8b196e50401462edcd7
--local <--local>
2020-09-28 12:49:47 -0400

    TCR

--- Tests/Utilities/test_email_utility.py
+++ Tests/Utilities/test_email_utility.py
@@ -11,14 +11,16 @@ import ssl
 def test_send_email():
     port = 465  # For SSL
     smtp_server = "smtp.gmail.com"
-    sender_email = "terngoodod@gmail.com"  # Enter your address
-    receiver_email = "terngoodod@gmail.com"  # Enter receiver address
+    sender_email = "anakwannaphaschaiyong@gmail.com"  # Enter your address
+    receiver_email = "anakwannaphaschaiyong@gmail.com"  # Enter receiver
+    # address
     # password = input("Type your password and press enter: ")
     message = """\
     Subject: Hi there
 
     This message is sent from Python."""
-    password = 'Terng2258'
+    # password = 'Terng2258'
+    password = 'Yeeha1234'
 
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
